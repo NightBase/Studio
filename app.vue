5d727198b4dcf6d3f10b04aafb20a05b3d2329b0
Fenish <sohretalhadev@outlook.com>
2023-08-12 02:45:48 +0300

    Implement loading screen

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
-  <NuxtPage />
+  <UtilsLoadingScreen v-if="isLoading" />
+  <NuxtPage v-else />
 </template>
+
+<script setup lang="ts">
+const isLoading = ref(true);
+onNuxtReady(() => {
+  isLoading.value = false;
+});
+</script>
