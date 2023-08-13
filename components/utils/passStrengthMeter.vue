e30bcfc849458a1dddc11956d2db36f983b7a4db
Fenish <sohretalhadev@outlook.com>
2023-08-14 01:47:48 +0300

    Add extra check for ultra strong passwords

--- components/utils/passStrengthMeter.vue
+++ components/utils/passStrengthMeter.vue
@@ -1,10 +1,64 @@
+<style scoped lang="scss">
+$bg-url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABxCAMAAABSkVXkAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAIFQTFRFAAAA////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Xd8iUwAAACt0Uk5TABL9/4jQkP4eUGCg4QGAMDh478Cw+AdoSCDgEFjIKEBw9wiYuNio7QKpEXVm9FgAAAGLSURBVHic7dlZbsJAEEVRGzODmW3mME/Z/wKD8pv7c9XYIol7AUcpqx7V1Ymi54lryc9Tj8RpANAwQBOAVlsAHQCSrgDwI/RMDfQR+qkABlTDUAAjAsYCSCcATAUQzQDIcgHMqYaFAJYENE0NKwDCu3ktAOzmjQDWLQBUN28BWBkAu/lDADsC9gLIqZtnpgbq5omJ9J5qGAlgSMBAAGkfABXpHgCHowC6VIOJdJu6WUW6kAFViwUQHOn4AICK9BSAKtJJFenvU0U6eUWkOwLAO+fJ1FDMlF4KACM9F8CRftdUpM8AVJF+nszcOTHSppuLifTFABRp1c0LAswamWcAXE0NNKVvdwGMqQazRmI37wWAa2TwlL6ZAYVr5E4AuEaaSOMauTUARVqtkRuqIXhAVZFOXhBpNaDeNNJnART0MvTLIn0C4A3unOVGuk41BN85TaTxzllupIMfezHS5T72XgEod40Mf+y9APAGa6R6GQr+18VfWCNvAPy3l6E7fQQ1oB70J3wKoOA18gvwXizr2/1dOgAAAABJRU5ErkJggg==";
+
+.purple-glow::before {
+  content: " ";
+  display: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 9999px;
+  animation: box-glow 1s ease-in-out infinite alternate;
+}
+.super-strong {
+  overflow: hidden;
+}
+.super-strong::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  display: block;
+  background-image: url($bg-url);
+  background-size: 40px;
+  opacity: 0.4;
+  object-fit: cover;
+  animation: bg-move 0.3s infinite linear;
+  overflow: hidden;
+}
+@keyframes bg-move {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 40px 0px;
+  }
+}
+@keyframes box-glow {
+  0% {
+    opacity: 0.6;
+    -webkit-box-shadow: 0px 0px 38px 2px rgba(220, 130, 250, 1);
+    -moz-box-shadow: 0px 0px 38px 2px rgba(220, 130, 250, 1);
+    box-shadow: 0px 0px 38px 2px rgba(220, 130, 250, 1);
+  }
+  100% {
+    opacity: 0.2;
+    -webkit-box-shadow: 0px 0px 23px 2px rgba(220, 130, 250, 1);
+    -moz-box-shadow: 0px 0px 23px 2px rgba(220, 130, 250, 1);
+    box-shadow: 0px 0px 23px 2px rgba(220, 130, 250, 1);
+  }
+}
+</style>
+
 <template>
   <div class="flex flex-col justify-center items-end gap-y-1">
     <div
-      class="bg-zinc-900 border border-zinc-800 w-full h-3 rounded-full flex items-center"
+      class="bg-zinc-900 w-full h-3 rounded-full flex relative items-center"
+      :class="strength === 'Super Strong!!' ? 'purple-glow' : ''"
     >
       <div
-        class="h-2 rounded-full transition-all duration-300"
+        class="h-full rounded-full transition-all duration-300 absolute"
+        :class="strength === 'Super Strong!!' ? 'super-strong' : ''"
         :style="{
           width: `${strengthPercent}%`,
           backgroundColor: strengthColor,
@@ -63,7 +117,7 @@ const strengthColor = computed(() => {
     case PasswordStrength.Strong:
       return "#81fc4c";
     case PasswordStrength.SuperStrong:
-      return "#4cfcf1";
+      return "#d36af3";
   }
 });
 
