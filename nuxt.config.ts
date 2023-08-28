44e53a09fc8879629908367ab38ec1571596dea4
Fenish <sohretalhadev@outlook.com>
2023-08-29 00:49:39 +0300

    Add api url to runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
-  devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+    devtools: { enabled: true },
+    runtimeConfig: {
+        public: {
+            API_URL: process.env.API_URL || 'http://localhost:3001',
+        },
+    },
 });
