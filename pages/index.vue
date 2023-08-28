82f88a9ffa4c05e4879681be0012a34856b4f671
Fenish <sohretalhadev@outlook.com>
2023-08-29 00:49:59 +0300

    Lint Code

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,67 @@
 <template>
-  <div
-    class="w-screen h-screen max-w-screen max-h-screen bg-primary-dark overflow-hidden"
-  >
-    <LoginBackEffects />
-    <div class="flex justify-center items-center w-full h-full">
-      <div class="flex items-center justify-center flex-col relative">
-        <div class="flex flex-col py-20 px-10 bg-zinc-800/60 rounded-xl">
-          <div class="flex mb-8 flex-col">
-            <div class="flex justify-center items-center mb-8">
-              <img
-                src="/logo.png"
-                width="128"
-                class="select-none"
-                draggable="false"
-              />
-            </div>
+    <div class="w-screen h-screen max-w-screen max-h-screen bg-primary-dark overflow-hidden">
+        <LoginBackEffects />
+        <div class="flex justify-center items-center w-full h-full">
+            <div class="flex items-center justify-center flex-col relative">
+                <div class="flex flex-col py-20 px-10 w-[465px] bg-zinc-800/60 rounded-xl">
+                    <div
+                        class="flex flex-col"
+                        :style="{
+                            'margin-bottom': errorVisible ? '1rem' : '2rem',
+                        }"
+                    >
+                        <div class="flex justify-center items-center mb-8">
+                            <img src="/logo.png" width="128" class="select-none" draggable="false" />
+                        </div>
 
-            <div class="flex flex-col">
-              <span class="text-white font-bold text-3xl"
-                >Welcome to NightBase</span
-              >
-              <span class="text-white font-light text-lg"
-                >In order to continue please sign in</span
-              >
+                        <div class="flex flex-col">
+                            <span class="text-white font-bold text-3xl">Welcome to NightBase</span>
+                            <span class="text-white font-light text-lg">In order to continue please sign in</span>
+                            <span v-if="errorVisible === true" class="text-center mt-3 text-red-500">{{
+                                errorMessage
+                            }}</span>
+                        </div>
+                    </div>
+                    <div class="w-full">
+                        <form class="flex flex-col gap-y-5" autocomplete="off" @submit.prevent="handleLogin">
+                            <LoginInput
+                                v-model="username"
+                                placeholder="Username or Email"
+                                icon="material-symbols:person-2-rounded"
+                            />
+                            <LoginInput v-model="password" placeholder="Password" icon="uim:padlock" is-password />
+                            <button
+                                type="submit"
+                                class="mt-5 bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 rounded-full"
+                            >
+                                Sign in
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="w-full">
-            <form class="flex flex-col gap-y-5" autocomplete="off">
-              <LoginInput
-                placeholder="Username or Email"
-                icon="material-symbols:person-2-rounded"
-              />
-              <LoginInput
-                placeholder="Password"
-                icon="uim:padlock"
-                isPassword
-              />
-              <button
-                type="submit"
-                class="mt-5 bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 rounded-full"
-              >
-                Sign in
-              </button>
-            </form>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-const username = ref("");
+const username = ref('');
+const password = ref('');
+
+const errorVisible = ref(false);
+const errorMessage = ref('');
+
+const { login } = useStudioAuth();
+
+async function handleLogin() {
+    try {
+        await login(username.value, password.value);
+    } catch (error: any) {
+        errorVisible.value = true;
+        errorMessage.value = error.message;
+        setTimeout(() => {
+            errorVisible.value = false;
+        }, 3000);
+    }
+}
 </script>
--- pages/setup.vue
+++ pages/setup.vue
@@ -1,124 +1,95 @@
 <template>
-  <div
-    class="w-screen h-screen max-w-screen max-h-screen bg-primary-dark overflow-hidden"
-  >
-    <LoginBackEffects />
-    <div class="flex justify-center items-center w-full h-full">
-      <div class="flex items-center justify-center flex-col relative">
-        <div class="flex flex-col py-20 px-10 bg-zinc-800/60 rounded-xl">
-          <div class="flex mb-3 flex-col">
-            <div class="flex justify-center items-center mb-8">
-              <img
-                src="/logo.png"
-                width="128"
-                class="select-none"
-                draggable="false"
-              />
-            </div>
+    <div class="w-screen h-screen max-w-screen max-h-screen bg-primary-dark overflow-hidden">
+        <LoginBackEffects />
+        <div class="flex justify-center items-center w-full h-full">
+            <div class="flex items-center justify-center flex-col relative">
+                <div class="flex flex-col py-20 px-10 bg-zinc-800/60 rounded-xl">
+                    <div class="flex mb-3 flex-col">
+                        <div class="flex justify-center items-center mb-8">
+                            <img src="/logo.png" width="128" class="select-none" draggable="false" />
+                        </div>
 
-            <div class="flex flex-col">
-              <span class="text-white font-bold text-3xl"
-                >Welcome to Setup</span
-              >
-              <span class="text-white font-light text-lg"
-                >Please register root user</span
-              >
-            </div>
-          </div>
-          <div class="mb-3">
-            <span class="text-red-500 text-xs" v-if="errorVisibility">
-              {{ errorText }}
-            </span>
-          </div>
-          <div class="w-full">
-            <form
-              class="flex flex-col gap-y-5"
-              autocomplete="off"
-              @submit.prevent="register"
-            >
-              <LoginInput
-                placeholder="Username"
-                icon="material-symbols:person-2-rounded"
-                v-model="username"
-              />
-              <LoginInput
-                type="email"
-                placeholder="Email"
-                icon="material-symbols:mail"
-                v-model="email"
-              />
-              <LoginInput
-                placeholder="Password"
-                icon="uim:padlock"
-                isPassword
-                v-model="password"
-              />
-              <div class="gap-y-3 flex flex-col">
-                <LoginInput
-                  placeholder="Re Enter Password"
-                  icon="uim:padlock"
-                  isPassword
-                  v-model="password2"
-                />
-                <UtilsPassStrengthMeter
-                  v-if="meterVisibility"
-                  :password="password2"
-                  v-model="passStrength"
-                />
-              </div>
+                        <div class="flex flex-col">
+                            <span class="text-white font-bold text-3xl">Welcome to Setup</span>
+                            <span class="text-white font-light text-lg">Please register root user</span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <span v-if="errorVisibility" class="text-red-500 text-xs">
+                            {{ errorText }}
+                        </span>
+                    </div>
+                    <div class="w-full">
+                        <form class="flex flex-col gap-y-5" autocomplete="off" @submit.prevent="register">
+                            <LoginInput
+                                v-model="username"
+                                placeholder="Username"
+                                icon="material-symbols:person-2-rounded"
+                            />
+                            <LoginInput v-model="email" type="email" placeholder="Email" icon="material-symbols:mail" />
+                            <LoginInput v-model="password" placeholder="Password" icon="uim:padlock" is-password />
+                            <div class="gap-y-3 flex flex-col">
+                                <LoginInput
+                                    v-model="password2"
+                                    placeholder="Re Enter Password"
+                                    icon="uim:padlock"
+                                    is-password
+                                />
+                                <UtilsPassStrengthMeter
+                                    v-if="meterVisibility"
+                                    v-model="passStrength"
+                                    :password="password2"
+                                />
+                            </div>
 
-              <button
-                type="submit"
-                class="mt-5 bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 rounded-full"
-              >
-                Register
-              </button>
-            </form>
-          </div>
+                            <button
+                                type="submit"
+                                class="mt-5 bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 rounded-full"
+                            >
+                                Register
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-const username = ref("");
-const email = ref("");
-const password = ref("");
-const password2 = ref("");
+const username = ref('');
+const email = ref('');
+const password = ref('');
+const password2 = ref('');
 
-const errorText = ref("");
+const errorText = ref('');
 const errorVisibility = ref(false);
 
-const passStrength = ref("Weak");
+const passStrength = ref('Weak');
 const meterVisibility = computed(() => {
-  return password2.value.length > 3;
+    return password2.value.length > 3;
 });
 
 watch([password, password2], (newVal) => {
-  if (newVal[0].length < 8 && newVal[0].length) {
-    errorVisibility.value = false;
-    passStrength.value = "Weak";
-    return;
-  }
-  if (
-    password.value !== password2.value &&
-    (password.value.length > 3 || password2.value.length > 3)
-  ) {
-    errorVisibility.value = true;
-    errorText.value = "Passwords do not match";
-  } else {
-    errorVisibility.value = false;
-    passStrength.value = "Weak";
-  }
+    if (newVal[0].length < 8 && newVal[0].length) {
+        errorVisibility.value = false;
+        passStrength.value = 'Weak';
+        return;
+    }
+    if (password.value !== password2.value && (password.value.length > 3 || password2.value.length > 3)) {
+        errorVisibility.value = true;
+        errorText.value = 'Passwords do not match';
+    } else {
+        errorVisibility.value = false;
+        passStrength.value = 'Weak';
+    }
 });
 
 function register() {
-  errorVisibility.value = false;
-  if (passStrength.value !== "Medium") {
-    errorVisibility.value = true;
-    errorText.value = "Password is not strong enough";
-    return;
-  }
+    errorVisibility.value = false;
+    if (passStrength.value !== 'Medium') {
+        errorVisibility.value = true;
+        errorText.value = 'Password is not strong enough';
+    }
 }
 </script>
