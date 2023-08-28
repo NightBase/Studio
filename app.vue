e8b157e3751106436428be5e660c991c2b537a1d
Fenish <sohretalhadev@outlook.com>
2023-08-29 00:49:25 +0300

    Lint app.vue

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <template>
-  <UtilsLoadingScreen v-if="isLoading" />
-  <KeepAlive v-else>
-    <div class="bg-primary-dark min-w-full min-h-screen">
-      <NuxtPage />
-    </div>
-  </KeepAlive>
+    <UtilsLoadingScreen v-if="isLoading" />
+    <KeepAlive v-else>
+        <div class="bg-primary-dark min-w-full min-h-screen">
+            <NuxtPage />
+        </div>
+    </KeepAlive>
 </template>
 
 <script setup lang="ts">
 const isLoading = ref(true);
 onNuxtReady(() => {
-  isLoading.value = false;
+    isLoading.value = false;
 });
 </script>
