3a9684ae983fcbd5ead3e0e488363e9fcd8a4012
Fenish <sohretalhadev@outlook.com>
2023-09-03 01:02:50 +0300

    Add database variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,14 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             API_URL: process.env.API_URL || 'http://localhost:3001',
+            DB_HOST: process.env.DB_HOST || 'localhost',
+            DB_PORT: process.env.DB_PORT || '5432',
+            DB_USER: process.env.DB_USER || 'nightbase',
+            DB_PASSWORD: process.env.DB_PASSWORD || 'nightbase',
+            DB_DATABASE: process.env.DB_DATABASE || 'nightbase',
         },
+    },
+    nitro: {
+        plugins: ['@/plugins/server/DBSetup.ts'],
     },
 });
